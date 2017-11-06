615068069883e41d4ee6d388ddb16b8a758963b6
Jonathan Cho <jonofcho@gmail.com>
2017-11-05 16:32:06 -0800

    create generic article model

---
+++ models/Article.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+var articleSchema = mongoose.Schema({
+  header:{
+    type: String,
+    required: true,
+  },
+  body:{
+    type: String,
+    required: true,
+  },
+  updatedDate:{
+    type: String,
+  },
+  createdDate:{
+    type: String,
+  },
+  createdBy:{
+    type: String,
+    required: true,
+  },
+  estimatedReadTime:{
+    type: String,
+    required: true,
+  },
+  views:{
+    type: Number,
+  },
+  deleted: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const User = module.exports = mongoose.model('Article' , articleSchema);
